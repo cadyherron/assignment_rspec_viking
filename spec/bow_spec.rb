7845f15b62737aa7d35cb6e25a9717f243e0bb39
Sam O'Keefe <samok13@gmail.com>
2016-01-08 13:46:42 -0800

    complete viking bow tests

--- spec/bow_spec.rb
+++ spec/bow_spec.rb
@@ -7,6 +7,7 @@ describe Bow do
   let( :bow ) { Bow.new }
   let( :bow_6 ) { Bow.new(6)}
 
+
   describe "#initialize" do
     it "make sure bow's arrow count is readable" do
       expect(bow).to respond_to :arrows
@@ -18,6 +19,14 @@ describe Bow do
 
     it "can initialize with 6 arrows" do
       expect(bow_6.arrows).to eq(6)
+    end
+
+    it "using a bow reduces arrows by 1" do
+      expect{bow.use}.to change(bow,:arrows).by(-1)
+    end
+
+    it "using bow with no arrows throw error" do
+      expect{11.times {bow.use}}.to raise_error("Out of arrows")
     end
 
   end
